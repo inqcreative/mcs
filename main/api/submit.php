657233572e754dc57b1154f453d8b377694b0b54
Jonathan Lee <jonodjlee.github@gmail.com>
2021-01-15 17:41:51 +1100

    Update submit.php

--- main/api/submit.php
+++ main/api/submit.php
@@ -98,7 +98,6 @@ function handle_transfer_ids_post($ids) {
 	<link rel="shortcut icon" href="" type="image/x-icon">
 	<link rel="apple-touch-icon" href="">
 
-	<!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<!-- Link to your css file -->
 	<link rel="stylesheet" href="">
