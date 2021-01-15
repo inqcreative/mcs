40442ebf5d80f4bf9b0a1be96b729e6498d6ee97
Jonathan Lee <jonodjlee.github@gmail.com>
2021-01-15 17:46:38 +1100

    Update submit.php

--- main/api/submit.php
+++ main/api/submit.php
@@ -86,6 +86,7 @@ function handle_transfer_ids_post($ids) {
 ?>
 
 <!DOCTYPE html>
+<!--
 <html lang="en">
 <head>
 	<meta charset="utf-8">
@@ -112,3 +113,4 @@ function handle_transfer_ids_post($ids) {
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
+-->
